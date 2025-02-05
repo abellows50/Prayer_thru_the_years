516a36c094a4878e5aca72bc106916f3ff5baeec
abellows50 <abellows50@stuy.edu>
2025-02-04 22:29:30 -0500

    added songs

---
+++ Prayer_thru_the_Year_by_Chaskele_Ritter/grab.sh
@@ -0,0 +1,78 @@
+curl --output 4f 'https://rsa.fau.edu/sites/all/modules/rsa_player/3343177.php?f=50463f3919d16316cc4f2ce13cb855e6' \
+  -H 'Accept: */*' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: has_js=1; _gid=GA1.2.917865205.1738722097; _pk_ref.1.0be4=%5B%22%22%2C%22%22%2C1738722097%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.0be4=*; SSESS3327beacb8de18a6d086c9ba373e6ee1=n6eFmeWcqWgIbyGkG0Km5F2O7OvZnbuo70o93qfgSi4; _ga_9D8NTH27GM=GS1.1.1738722096.1.1.1738725038.0.0.0; _ga=GA1.2.1079993399.1738722097; _pk_id.1.0be4=388a52c2203464d7.1738722097.1.1738725039.1738722097.' \
+  -H 'Range: bytes=0-' \
+  -H 'Referer: https://rsa.fau.edu/album/42479' \
+  -H 'Sec-Fetch-Dest: audio' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"';
+
+  curl --output 5f 'https://rsa.fau.edu/sites/all/modules/rsa_player/3343177.php?f=e490280669356066ec44b9f2a41763ab' \
+    -H 'Accept: */*' \
+    -H 'Accept-Language: en-US,en;q=0.9' \
+    -H 'Connection: keep-alive' \
+    -H 'Cookie: has_js=1; _gid=GA1.2.917865205.1738722097; _pk_ref.1.0be4=%5B%22%22%2C%22%22%2C1738722097%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.0be4=*; SSESS3327beacb8de18a6d086c9ba373e6ee1=n6eFmeWcqWgIbyGkG0Km5F2O7OvZnbuo70o93qfgSi4; _ga_9D8NTH27GM=GS1.1.1738722096.1.1.1738725038.0.0.0; _ga=GA1.2.1079993399.1738722097; _pk_id.1.0be4=388a52c2203464d7.1738722097.1.1738725039.1738722097.' \
+    -H 'If-Modified-Since: Mon, 26 Jun 2017 15:38:51 -0400' \
+    -H 'Range: bytes=0-1048575' \
+    -H 'Referer: https://rsa.fau.edu/album/42479' \
+    -H 'Sec-Fetch-Dest: audio' \
+    -H 'Sec-Fetch-Mode: no-cors' \
+    -H 'Sec-Fetch-Site: same-origin' \
+    -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' \
+    -H 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"' \
+    -H 'sec-ch-ua-mobile: ?0' \
+    -H 'sec-ch-ua-platform: "Windows"';
+
+    curl --output 6f 'https://rsa.fau.edu/sites/all/modules/rsa_player/3343177.php?f=a267fc646f3c346533ea9c58786fb0a8' \
+      -H 'Accept: */*' \
+      -H 'Accept-Language: en-US,en;q=0.9' \
+      -H 'Connection: keep-alive' \
+      -H 'Cookie: has_js=1; _gid=GA1.2.917865205.1738722097; _pk_ref.1.0be4=%5B%22%22%2C%22%22%2C1738722097%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.0be4=*; SSESS3327beacb8de18a6d086c9ba373e6ee1=n6eFmeWcqWgIbyGkG0Km5F2O7OvZnbuo70o93qfgSi4; _ga_9D8NTH27GM=GS1.1.1738722096.1.1.1738725038.0.0.0; _ga=GA1.2.1079993399.1738722097; _pk_id.1.0be4=388a52c2203464d7.1738722097.1.1738725039.1738722097.' \
+      -H 'If-Modified-Since: Mon, 26 Jun 2017 15:39:35 -0400' \
+      -H 'Range: bytes=0-1048575' \
+      -H 'Referer: https://rsa.fau.edu/album/42479' \
+      -H 'Sec-Fetch-Dest: audio' \
+      -H 'Sec-Fetch-Mode: no-cors' \
+      -H 'Sec-Fetch-Site: same-origin' \
+      -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' \
+      -H 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"' \
+      -H 'sec-ch-ua-mobile: ?0' \
+      -H 'sec-ch-ua-platform: "Windows"';
+
+      curl --output 7f 'https://rsa.fau.edu/sites/all/modules/rsa_player/3343177.php?f=d88deabcce46d2d2c34f0073156e96b3' \
+        -H 'Accept: */*' \
+        -H 'Accept-Language: en-US,en;q=0.9' \
+        -H 'Connection: keep-alive' \
+        -H 'Cookie: has_js=1; _gid=GA1.2.917865205.1738722097; _pk_ref.1.0be4=%5B%22%22%2C%22%22%2C1738722097%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.0be4=*; SSESS3327beacb8de18a6d086c9ba373e6ee1=n6eFmeWcqWgIbyGkG0Km5F2O7OvZnbuo70o93qfgSi4; _ga_9D8NTH27GM=GS1.1.1738722096.1.1.1738725038.0.0.0; _ga=GA1.2.1079993399.1738722097; _pk_id.1.0be4=388a52c2203464d7.1738722097.1.1738725039.1738722097.' \
+        -H 'If-Modified-Since: Mon, 26 Jun 2017 15:40:08 -0400' \
+        -H 'Range: bytes=0-1048575' \
+        -H 'Referer: https://rsa.fau.edu/album/42479' \
+        -H 'Sec-Fetch-Dest: audio' \
+        -H 'Sec-Fetch-Mode: no-cors' \
+        -H 'Sec-Fetch-Site: same-origin' \
+        -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' \
+        -H 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"' \
+        -H 'sec-ch-ua-mobile: ?0' \
+        -H 'sec-ch-ua-platform: "Windows"';
+curl --output 8f 'https://rsa.fau.edu/sites/all/modules/rsa_player/3343177.php?f=094c940b307c0b8849ea502e4aedfba7' \
+  -H 'Accept: */*' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: has_js=1; _gid=GA1.2.917865205.1738722097; _pk_ref.1.0be4=%5B%22%22%2C%22%22%2C1738722097%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.0be4=*; SSESS3327beacb8de18a6d086c9ba373e6ee1=n6eFmeWcqWgIbyGkG0Km5F2O7OvZnbuo70o93qfgSi4; _ga_9D8NTH27GM=GS1.1.1738722096.1.1.1738725038.0.0.0; _ga=GA1.2.1079993399.1738722097; _pk_id.1.0be4=388a52c2203464d7.1738722097.1.1738725039.1738722097.' \
+  -H 'If-Modified-Since: Mon, 26 Jun 2017 15:41:51 -0400' \
+  -H 'Range: bytes=0-1048575' \
+  -H 'Referer: https://rsa.fau.edu/album/42479' \
+  -H 'Sec-Fetch-Dest: audio' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"';
+  
